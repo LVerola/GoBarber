834f0f83dc3641ad6af84d741f6f00123654a854
user <luisgabriel.verola@gmail.com>
2020-07-17 14:25:20 -0300

    Loader de models

--- NODE/src/routes.js
+++ NODE/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Luis' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luis Gabriel',
+    email: 'luisgabriel.verola@gmail.com',
+    password_hash: '12364616548',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
