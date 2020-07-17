f2fcdaaa2ce10aa2f5f52b03c529fcce63193245
user <luisgabriel.verola@gmail.com>
2020-07-17 14:57:30 -0300

    Cadastro de Usuários

--- NODE/src/routes.js
+++ NODE/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Luis Gabriel',
-    email: 'luisgabriel.verola@gmail.com',
-    password_hash: '12364616548',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
