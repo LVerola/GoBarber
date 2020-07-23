2c378d172576569a58133e16bb95f5066f76d706
user <luisgabriel.verola@gmail.com>
2020-07-23 17:10:46 -0300

    Configurando Nodemailer

---
+++ NODE/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '96af2b489512d0',
+    pass: 'fa44841b125f95',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
