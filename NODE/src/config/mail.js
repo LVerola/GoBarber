e19dfed7e5cacca20dd724ca8099459c46a47045
user <luisgabriel.verola@gmail.com>
2020-07-24 16:37:16 -0300

    Variáveis ambiente

--- NODE/src/config/mail.js
+++ NODE/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '96af2b489512d0',
-    pass: 'fa44841b125f95',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
--- NODE/src/config/sentry.js
+++ NODE/src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-  dsn:
-    'https://7dc5d50a895844dc9821b5d2c618a975@o425127.ingest.sentry.io/5357853',
+  dsn: process.env.SENTRY_DSN,
 };
